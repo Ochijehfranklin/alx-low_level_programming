b04bc4268dae7f2a11f20102f26abf77132b5fd3
Ochijehfranklin <ochijehfranklin@gmail.com>
2023-07-16 22:25:32 -0700

    CSoftware is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point
+ * description: 'print four digits'
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for (int x = 0; x <= 98; x++)
+	{
+		for (int y = x + 1; y <= 99; y++)
+		{
+			putchar('0' + x / 10);
+			putchar('0' + x % 10);
+			putchar(' ');
+			putchar('0' + y / 10);
+			putchar('0' + y % 10);
+			if (x != 98 || y != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
