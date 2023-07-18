4d7bd82464e550166c97b0dd59f72166a3431462
Ochijehfranklin <ochijehfranklin@gmail.com>
2023-07-18 07:56:19 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - This is the entry point
+ * Description: Prints alphabets 10 times
+ * Return: Always 0;
+ */
+
+void print_alphabet_x10(void);
+{
+	int x;
+	int y;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (y = 97; y <= 122; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
