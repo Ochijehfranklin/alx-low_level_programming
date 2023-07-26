48581026a3742d51d6c0a63348cb0ef93e3ba9e4
Ochijehfranklin <ochijehfranklin@gmail.com>
2023-07-26 01:11:22 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - This function copies a sting to another
+ *
+ * @dest: This would hold the other value
+ *
+ * @src: This is the string that would copied to another
+ *
+ * Return: a character
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,5 +5,5 @@
 #include <unistd.h>
 
 int _putchar(char c);
-
+char *_strcat(char *dest, char *src);
 #endif /*MAIN_H*/
