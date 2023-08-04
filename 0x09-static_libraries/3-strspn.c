07242bcf660e3d882116bae607df527dbb0c33e5
Ochijehfranklin <ochijehfranklin@gmail.com>
2023-08-04 09:44:43 -0700

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -26,11 +26,9 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 			else if (accept[r + 1] == '\0')
-			{
 				return (n);
-			}
-			s++;
 		}
-		return (n);
+			s++;
 	}
+	return (n);
 }
