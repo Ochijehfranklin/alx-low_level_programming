f9a2acce4f8af2e63790e86e533c4697163fe443
Ochijehfranklin <ochijehfranklin@gmail.com>
2023-08-29 05:50:34 -0700

    sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,20 @@
 #include "lists.h"
+
+/**
+ * sum_listint - Calculates the sum of all the data in a listint_t list
+ * @head: pointer to the first node
+ * Return: Total of all int in linked list
+ */
+
+int sum_listint(listint_t *head)
+{
+	int total = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		total += temp->n;
+		temp = temp->next;
+	}
+	return (total);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
+int sum_listint(listint_t *head);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
