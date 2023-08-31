d73ee8ab1f906dfa2512ac969034fb4a6919e1b1
Ochijehfranklin <ochijehfranklin@gmail.com>
2023-08-31 10:13:18 -0700

    Find the loop

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converst binary to unsigned int
+ * @b: pointer to char 0 and 1
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int x;
+
+	if (!b)
+	{
+		return (0);
+	}
+
+	for (x = 0; x < b[x]; x++)
+	{
+		if (b[x] < '0' || b[x] > '1')
+		{
+			return (0);
+		}
+		else
+		{
+			result = 2 * result + (b[x] - '0');
+		}
+	}
+	return (result);
+}
