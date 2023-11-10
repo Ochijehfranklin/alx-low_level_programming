fbcdfd75d2ad135e59e081d80f7c9d6b98979b0e
Ochijehfranklin <ochijehfranklin@gmail.com>
2023-11-09 17:45:14 -0800

    list length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elemetns in list
+ * @h: pointer to first element in list
+ * Return: Numbwer of elements in list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	char count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
